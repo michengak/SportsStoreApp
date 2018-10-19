96fd775aff5064493172f269f050df67f603c463
michel ngako <emoumichel@gmail.com>
2018-10-19 10:18:29 -0400

    Improving URLs

--- SportsStore.WebUI/Models/ProductsListViewModel.cs
+++ SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using SportsStore.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Models
 {
     public class ProductsListViewModel
     {
         public IEnumerable<Product> Products { get; set; }
-        public PagingInfo PagingInfo { get; set; }
+        public PagingInfo PagingInfo { get; set;}
     }
 }
