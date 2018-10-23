72cc486a8c907b67f84e02d53ea63e2a341ec1a4
michel ngako <emoumichel@gmail.com>
2018-10-23 11:44:01 -0400

    Creating the view, highlighting the current category creating unit test for reporting the selected category

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class NavController : Controller
     {
-        // GET: Nav
-        public string Menu()
+        private IProductsRepository repository;
+
+        public NavController(IProductsRepository repo)
         {
-            return "Hello from NavController";
+            repository = repo;
+        }
+
+        //public PartialViewResult Menu(string category = null) => ViewBag.SelectedCategory = category;
+
+        // GET: Nav
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products
+                                       .Select(x => x.Category)
+                                       .Distinct()
+                                       .OrderBy(x => x);
+            return PartialView(categories);
         }
     }
 }
