0fb142df1ac44e22ea31ed646b825b34db1a87f7
michel ngako <emoumichel@gmail.com>
2018-10-29 14:34:21 -0400

    Reducing Duplication in the checkout.cshtlm file

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -70,5 +70,10 @@ namespace SportsStore.WebUI.Controllers
         {
             return PartialView(cart);
         }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
     }
 }
